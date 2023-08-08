562e097ccc8cc406cc69c54bd6b0cc894b5de423
João Pedro Araujo Queiroz Barbosa <111623525+joaopedroaqb@users.noreply.github.com>
2023-08-08 00:57:38 -0300

    Add files via upload

---
+++ Site-vendas/scripts/script.js
@@ -0,0 +1,8 @@
+// Captura o botão pelo seu ID
+const botaoInterativo = document.getElementById('botao-interativo');
+
+// Adiciona um ouvinte de evento de clique no botão
+botaoInterativo.addEventListener('click', function() {
+  // Exibe um alerta quando o botão é clicado
+  alert('Você clicou no botão interativo!');
+});
